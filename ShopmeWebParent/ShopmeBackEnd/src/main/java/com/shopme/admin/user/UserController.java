fcc4367433acf1dcc7a004b6c3c4fbdfd6df5d67
HaozheWang <haozhe.wang.chn@outlook.com>
2022-08-12 02:19:31 -0400

    Create New User Input

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -18,5 +19,18 @@ public class UserController {
         List<User> listUsers = service.listAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        System.out.println(user);
+        return "redirect:/users";
     }
 }

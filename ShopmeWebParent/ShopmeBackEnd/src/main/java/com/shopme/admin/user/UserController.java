3c8b2238ade02653d0fcfe502a5e8356183de90f
HaozheWang <haozhe.wang.chn@outlook.com>
2022-08-13 01:19:23 -0400

    New User Roles & Save & Cancel

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,11 +1,13 @@
 package com.shopme.admin.user;
 
 import com.shopme.common.entity.User;
+import com.shopme.common.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,14 +25,19 @@ public class UserController {
 
     @GetMapping("/users/new")
     public String newUser(Model model){
+        List<Role> listRoles = service.listRoles();
         User user = new User();
+        user.setEnabled(true);
         model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
         return "user_form";
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user){
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
         System.out.println(user);
+        service.save(user);
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
         return "redirect:/users";
     }
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,6 +1,7 @@
 package com.shopme.admin.user;
 
 import com.shopme.common.entity.User;
+import com.shopme.common.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,20 @@ import java.util.List;
 @Service
 public class UserService {
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+
+    @Autowired
+    private RoleRepository roleRepo;
 
     public List<User> listAll(){
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepo.findAll();
+    }
+
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepo.findAll();
+    }
+
+    public void save(User user){
+        userRepo.save(user);
     }
 }

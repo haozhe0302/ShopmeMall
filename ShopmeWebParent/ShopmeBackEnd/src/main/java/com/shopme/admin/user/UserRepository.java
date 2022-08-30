5011ae9a9890f43ad8a0b27de3b1cc53b14f8a1d
HaozheWang <haozhe.wang.chn@outlook.com>
2022-08-30 02:02:17 -0400

    User Delete Implement

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email")
     public User getUserByEmail(@Param("email") String email);
+
+    public Long countById(Integer id);
 }

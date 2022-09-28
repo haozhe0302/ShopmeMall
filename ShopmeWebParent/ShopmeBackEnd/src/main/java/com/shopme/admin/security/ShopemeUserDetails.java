8313c823513e06891cba6372dfae763dcc4431be
HaozheWang <haozhe.wang.chn@outlook.com>
2022-09-28 01:55:26 -0400

    User Account Icon & Logout Dropdown Link

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopemeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopemeUserDetails.java
@@ -56,4 +56,8 @@ public class ShopemeUserDetails implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+
+    public String getFullname() {
+        return this.user.getFirstName() + " " + this.user.getLastName();
+    }
 }

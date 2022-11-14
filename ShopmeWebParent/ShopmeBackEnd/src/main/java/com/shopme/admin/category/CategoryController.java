63968a1e0e77436eec4093e631108efda29f5270
HaozheWang <haozhe.wang.chn@outlook.com>
2022-11-14 03:14:45 -0500

    Category Service & Controller & html

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -0,0 +1,23 @@
+package com.shopme.admin.category;
+
+import com.shopme.common.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+
+@Controller
+public class CategoryController {
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping("/categories")
+    public String listAll(Model model) {
+        List<Category> listCategories = service.listAll();
+        model.addAttribute("listCategories", listCategories);
+
+        return "categories/categories";
+    }
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -0,0 +1,17 @@
+package com.shopme.admin.category;
+
+import com.shopme.common.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repo;
+
+    public List<Category> listAll() {
+        return (List<Category>) repo.findAll();
+    }
+}

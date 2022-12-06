99fdba626d653dd66b9e81b93e3bb0669b2ac4f8
HaozheWang <haozhe.wang.chn@outlook.com>
2022-12-06 01:07:28 -0500

    Check Category Unique Test
    Check Category Uniqueness Test Methods

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/category/CategoryServiceTests.java
@@ -0,0 +1,124 @@
+package com.shopme.admin.category;
+
+import com.shopme.common.entity.Category;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+public class CategoryServiceTests {
+    @MockBean
+    private CategoryRepository repo;
+
+    @InjectMocks
+    private CategoryService service;
+
+    @Test
+    public void testCheckUniqueInNewModeReturnDuplicateName() {
+        // Duplicate category name
+        Integer id = null;
+        String name = "Computers";
+        String alias = "abc";
+
+        Category category = new Category(id, name, alias);
+
+        Mockito.when(repo.findByName(name)).thenReturn(category);
+        Mockito.when(repo.findByAlias(alias)).thenReturn(null);
+
+        String result = service.checkUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("DuplicateName");
+    }
+
+    @Test
+    public void testCheckUniqueInNewModeReturnDuplicateAlias() {
+        // Duplicate category alias
+        Integer id = null;
+        String name = "ABC";
+        String alias = "computers";
+
+        Category category = new Category(id, name, alias);
+
+        Mockito.when(repo.findByName(name)).thenReturn(null);
+        Mockito.when(repo.findByAlias(alias)).thenReturn(category);
+
+        String result = service.checkUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("DuplicateAlias");
+    }
+
+    @Test
+    public void testCheckUniqueInNewModeReturnOK() {
+        // No duplicate category exists
+        Integer id = null;
+        String name = "ABC";
+        String alias = "computers";
+
+        Category category = new Category(id, name, alias);
+
+        Mockito.when(repo.findByName(name)).thenReturn(null);
+        Mockito.when(repo.findByAlias(alias)).thenReturn(null);
+
+        String result = service.checkUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("OK");
+    }
+
+    @Test
+    public void testCheckUniqueInEditModeReturnDuplicateName() {
+        // Edit mode with id
+        Integer id = 1;
+        String name = "Computers";
+        String alias = "abc";
+
+        Category category = new Category(2, name, alias);
+
+        Mockito.when(repo.findByName(name)).thenReturn(category);
+        Mockito.when(repo.findByAlias(alias)).thenReturn(null);
+
+        String result = service.checkUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("DuplicateName");
+    }
+
+    @Test
+    public void testCheckUniqueInEditModeReturnDuplicateAlias() {
+        // Edit mode with id
+        Integer id = 1;
+        String name = "ABC";
+        String alias = "computers";
+
+        Category category = new Category(2, name, alias);
+
+        Mockito.when(repo.findByName(name)).thenReturn(null);
+        Mockito.when(repo.findByAlias(alias)).thenReturn(category);
+
+        String result = service.checkUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("DuplicateAlias");
+    }
+
+    @Test
+    public void testCheckUniqueInEditModeReturnOK() {
+        // Edit mode with id
+        Integer id = 1;
+        String name = "ABC";
+        String alias = "computers";
+
+        Category category = new Category(id, name, alias);
+
+        Mockito.when(repo.findByName(name)).thenReturn(null);
+        Mockito.when(repo.findByAlias(alias)).thenReturn(null);
+
+        String result = service.checkUnique(id, name, alias);
+
+        assertThat(result).isEqualTo("OK");
+    }
+}

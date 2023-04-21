ceffe460cfb53810813078ce7961c8d47d77dea3
HaozheWang <haozhe.wang.chn@outlook.com>
2023-04-21 16:36:14 -0400

    Brand Entity

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -0,0 +1,54 @@
+package com.shopme.common.entity;
+
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "brands")
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 128)
+    private String logo;
+
+    @ManyToMany
+    @JoinTable(name = "brands_categories", joinColumns = @JoinColumn(name = "brand_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+}

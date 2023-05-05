d671f22a11a679aa6a6e603d04f2daefdc34c01f
HaozheWang <haozhe.wang.chn@outlook.com>
2023-05-04 23:14:24 -0400

    Product Overview Form Page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRestController.java
@@ -1,7 +1,11 @@
 package com.shopme.admin.brand;
 
+import java.util.*;
+import com.shopme.common.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,24 @@ public class BrandRestController {
     @PostMapping("/brands/check_unique")
     public String checkUnique(@Param("id") Integer id, @Param("name") String name) {
         return service.checkUnique(id, name);
+    }
+
+    @GetMapping("/brands/{id}/categories")
+    public List<CategoryDTO> listCategoriesByBrand(@PathVariable(name = "id") Integer brandId) throws BrandNotFoundRestException {
+        List<CategoryDTO> listCategories = new ArrayList<>();
+
+        try {
+            Brand brand = service.get(brandId);
+            Set<Category> categories = brand.getCategories();
+
+            for (Category category : categories) {
+                CategoryDTO dto = new CategoryDTO(category.getId(), category.getName());
+                listCategories.add(dto);
+            }
+
+            return listCategories;
+        } catch (BrandNotFoundException e) {
+            throw new BrandNotFoundRestException();
+        }
     }
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -1,18 +1,21 @@
 package com.shopme.admin.product;
 
+import java.util.*;
 import com.shopme.common.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import com.shopme.admin.brand.BrandService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private BrandService brandService;
 
     @GetMapping("/products")
     public String listFirstPage(Model model) {
@@ -25,5 +28,30 @@ public class ProductController {
         model.addAttribute("listProducts", listProducts);
 
         return "products/products";
+    }
+
+    @GetMapping("/products/new")
+    public String newProduct(Model model) {
+        List<Brand> listBrands = brandService.listAll();
+
+        Product product = new Product();
+        product.setEnabled(true);
+        product.setInStock(true);
+
+        model.addAttribute("product", product);
+        model.addAttribute("listBrands", listBrands);
+        model.addAttribute("pageTitle", "Create New Product");
+
+        return "products/product_form";
+    }
+
+    @PostMapping("/products/save")
+    public String saveProduct(Product product) {
+
+        System.out.println("Product Name: " + product.getName());
+        System.out.println("Brand Name: " + product.getBrand().getName());
+        System.out.println("Category Name: " + product.getCategory().getName());
+
+        return "redirect:/products";
     }
 }
